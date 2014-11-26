dfd96d1673665cd7d2c766d847a301262182f743
marcoscirea <marco.prolog@gmail.com>
2014-11-26 11:27:58 +0100

    kinda works

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -5,7 +5,7 @@ public class Gravity : MonoBehaviour
 {
 
 	GameObject[] ships;
-	float g;
+	float g = (float) 6.67384 * Mathf.Pow(10f,-11f);
 
 	// Use this for initialization
 	void Start()
@@ -18,7 +18,14 @@ public class Gravity : MonoBehaviour
 	{
 		foreach (GameObject s in ships) {
 			float f = g * (this.rigidbody.mass * s.rigidbody.mass) / Mathf.Pow(Vector3.Distance(this.transform.position, s.transform.position), 2f);
-			s.rigidbody.AddForce( Vector3.Normalize(s.transform.position - this.transform.position) * f);
+			Debug.Log(f);
+
+			Vector3 direction = -(s.rigidbody.transform.position - transform.position);
+
+			s.rigidbody.constantForce.force = direction;
+			//s.rigidbody.AddForce(Vector3.Normalize(s.transform.position - this.transform.position) * f);
+
+			Debug.DrawLine (s.transform.position, s.transform.position + direction, Color.red);
 		}
 	}
 }
---
+++ Assets/Ship.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ship : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = new Vector3(10,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

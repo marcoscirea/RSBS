b340509d971ea65cad4689fdbb796d6c1953b719
marcoscirea <marco.prolog@gmail.com>
2014-11-26 09:45:16 +0100

    First proto scene

---
+++ Assets/Gravity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour
+{
+
+	GameObject[] ships;
+	float g;
+
+	// Use this for initialization
+	void Start()
+	{
+		ships = GameObject.FindGameObjectsWithTag("Ship");
+	}
+    
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		foreach (GameObject s in ships) {
+			float f = g * (this.rigidbody.mass * s.rigidbody.mass) / Mathf.Pow(Vector3.Distance(this.transform.position, s.transform.position), 2f);
+			s.rigidbody.AddForce( Vector3.Normalize(s.transform.position - this.transform.position) * f);
+		}
+	}
+}
